b7cb2e1e1a0aef7d24ac36a4990c237cfbf03812
Aila Lu <ailalutw@gmail.com>
2023-06-09 10:32:52 -0700

    Add User input validation w express-validator package on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,10 +4,29 @@ const bcrypt = require("bcryptjs");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
+const validateSignup = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
+];
+
 // Sign up
-router.post("/", async (req, res) => {
+router.post("/", validateSignup, async (req, res) => {
   const { email, password, username } = req.body; //deconstruct the request body
   const hashedPassword = bcrypt.hashSync(password); //use bcrypt's hashSync function to hash the user's provided password to be saved as the user's hashedPassword in the database
   const user = await User.create({ email, username, hashedPassword }); //Create a new User
