7627466c42cb356a6b6b96c8a27da04eae0b81da
Aila Lu <ailalutw@gmail.com>
2023-06-13 13:53:42 -0700

    Get all Spots owned by the Current User

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,24 +7,8 @@ const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
-const router = express.Router();
 
-// Restore session user
-router.get("/", (req, res) => {
-  const { user } = req;
-  if (user) {
-    const safeUser = {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      username: user.username,
-    };
-    return res.json({
-      user: safeUser,
-    });
-  } else return res.json({ user: null });
-});
+const router = express.Router();
 
 // The validateLogin middleware checks to see whether or not req.body.credential and req.body.password are empty. If one of them is empty, then an error will be returned as the response.
 const validateLogin = [
@@ -45,8 +29,6 @@ router.post("/", validateLogin, async (req, res, next) => {
   const user = await User.unscoped().findOne({
     where: {
       [Op.or]: {
-        firstName: user.firstName,
-        lastName: user.lastName,
         username: credential,
         email: credential,
       },
@@ -63,6 +45,8 @@ router.post("/", validateLogin, async (req, res, next) => {
 
   const safeUser = {
     id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
     email: user.email,
     username: user.username,
   };
@@ -74,10 +58,27 @@ router.post("/", validateLogin, async (req, res, next) => {
   });
 });
 
-// Log out: remove the token cookie from the response and return a JSON success message.
+//logout
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
+});
+
+//restore
+router.get("/", (req, res) => {
+  const { user } = req;
+  if (user) {
+    const safeUser = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username,
+    };
+    return res.json({
+      user: safeUser,
+    });
+  } else return res.json({ user: null });
 });
 
 module.exports = router;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -7,6 +7,7 @@ const { handleValidationErrors } = require("../../utils/validation");
 const { Spot, User, Booking, SpotImage, Review } = require("../../db/models");
 const router = express.Router();
 
+//Get all Spots
 router.get("/", async (req, res) => {
   const spots = await Spot.findAll({
     include: [{ model: Review }, { model: SpotImage, attributes: ["url"] }],
@@ -26,7 +27,7 @@ router.get("/", async (req, res) => {
     const avgRating = totalRating / reviews.length;
     spotJson.avgRating = avgRating;
     // console.log(spot);
-    if (spotJson.SpotImages.length > 0) {
+    if (spotJson.SpotImages.length) {
       spotJson.previewImage = spotJson.SpotImages[0].url;
     }
     delete spotJson.Reviews;
@@ -34,8 +35,40 @@ router.get("/", async (req, res) => {
 
     return spotJson;
   });
-  console.log(spotWithRatings);
+  // console.log(spotWithRatings);
   res.json({ Spots: spotWithRatings });
 });
 
+//Get all Spots owned by the Current User
+router.get("/current", requireAuth, async (req, res) => {
+  let spots = await Spot.findAll({
+    include: [{ model: Review }, { model: SpotImage, attributes: ["url"] }],
+    where: { ownerId: req.user.id },
+  });
+
+  let userSpots = spots.map((spot) => {
+    let spotJson = spot.toJSON();
+    // console.log(spot);
+    let totalRating = 0;
+    let reviews = spotJson.Reviews;
+    // console.log(reviews);
+
+    reviews = reviews.map((review) => {
+      totalRating += review.stars;
+      // console.log(totalRating);
+    });
+    const avgRating = totalRating / reviews.length;
+    spotJson.avgRating = avgRating;
+    // console.log(spot);
+    if (spotJson.SpotImages.length) {
+      spotJson.previewImage = spotJson.SpotImages[0].url;
+    }
+    delete spotJson.Reviews;
+    delete spotJson.SpotImages;
+
+    return spotJson;
+  });
+  // console.log(userSpots);
+  res.json({ Spots: userSpots });
+});
 module.exports = router;
