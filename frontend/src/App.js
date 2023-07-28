fc7f9a5e81c2bc9b1a61cf0531c24705b45c3bc1
Aila Lu <ailalutw@gmail.com>
2023-07-27 22:18:27 -0700

    LoginFormPage done without css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <h1>Hello from Corner</h1>
+      {/* <Navigation /> */}
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+        <Route path="">{/* component */}</Route>
+      </Switch>
+    </>
   );
 }
 
