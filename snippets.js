3d5784a280963d75b2a5b80d9a53aeb6bbc91c8b
Aila Lu <ailalutw@gmail.com>
2023-07-28 21:39:44 -0700

    creating modal

--- snippets.js
+++ snippets.js
@@ -17,3 +17,16 @@ onClick((event) => {
   // evaluates to true if click happened inside of the element
   // evaluates to false if click happened outside of the element
 });
+
+// Let's say you want to render a popup modal text of "Hello World!" when a button with the text of "Greeting" is clicked. You can use the OpenModalButton to create a component which renders this button that triggers this modal.
+// The Greeting component will render a button element that, when clicked, will trigger a modal with an h2 element of "Hello World!" and will print "Greeting initiated" to the console logs. When the modal is closed, it will print "Greeting completed" to the console logs.
+const Greeting = () => {
+  return (
+    <OpenModalButton
+      buttonText="Greeting"
+      modalComponent={<h2>Hello World!</h2>}
+      onButtonClick={() => console.log("Greeting initiated")}
+      onModalClose={() => console.log("Greeting completed")}
+    />
+  );
+};
