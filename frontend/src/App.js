f73a5417a67d90feb332daa9964a4cf1c4a37568
Aila Lu <ailalutw@gmail.com>
2023-07-29 12:47:03 -0700

    trying out all spots

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+// import AllSpots from "./components/Spots/AllSpots";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route path="/">{/* <AllSpots /> */}</Route>
           <Route path="/login"></Route>
           <Route path="/signup"></Route>
         </Switch>
---
+++ frontend/src/components/Spots/Allspots/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AllSpots() {
+  return (
+    <>
+      <h1>AllSpots</h1>
+    </>
+  );
+}
+
+export default AllSpots;
